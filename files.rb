42a7f71da2ccecd3a2f7215e0b8e67341f07d77f
Ryoji Kobori <kobori75@gmail.com>
2014-02-07 03:19:07 +0900

    YAML sample added

--- files.rb
+++ files.rb
@@ -19,3 +19,54 @@ Dir.glob("**/*.rb").each do |file|
   count += 1
 end
 puts "[file count = #{count}]"
+
+# yamlの基本操作
+require 'yaml'
+
+puts PARTING_LINE
+elements = [
+  { "name" => "hoge",
+    "country" => "fuga",
+    "age" => 14,
+    "hobby" => ["game", "bycycle", "cooking"]
+  }
+]
+puts YAML.dump(elements)
+
+open("elements.yml", "w") do |file|
+  YAML.dump(elements, file)
+end
+
+puts PARTING_LINE
+loaded = YAML.load_file("elements.yml")
+puts loaded
+
+# RailsのMySQL用のDB設定の出力、読み込み
+db_config = {
+  "development" => 
+  { "adapter" => "mysql2",
+    "encoding" => "utf8",
+    "database" => "develop",
+    "pool" => 5,
+    "username" => "root",
+    "password" => "",
+    "host" => "localhost"
+  }
+}
+open("database.yml", "w") do |file|
+  YAML.dump(db_config, file)
+end
+
+puts PARTING_LINE
+database = YAML.load_file("database.yml")
+params = [
+  "adapter",
+  "encoding",
+  "database",
+  "pool",
+  "username",
+  "password",
+  "host"]
+params.each do |param|
+  puts "#{param}: #{database["development"][param]}"
+end
